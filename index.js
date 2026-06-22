7e67173ad92e63ef609b34cfddbca2ca72fa38fe
delwer72 <mddelwerhussain93@gmail.com>
2026-06-22 16:07:02 +0600

    port and stripe connected to env file

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ console.log("Stripe key prefix:", process.env.STRIPE_SECRET_KEY?.slice(0, 12));
 
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+const stripeInstance = stripe(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors({
   credentials: true,
